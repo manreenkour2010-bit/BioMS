fb97fbfe65d6988fb62d340e948f56af540cd6da
manreenkour2010-bit <manreenkour2010@gmail.com>
2025-12-16 18:04:20 -0800

    First Commit

---
+++ app/src/main/java/com/example/bioms1/camera.java
@@ -0,0 +1,28 @@
+package com.example.bioms1;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bioms1.databinding.ActivityCameraBinding;
+
+public class camera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera);
+    }
+
+}
---
+++ app/src/main/java/com/example/bioms1/demoSession.java
@@ -0,0 +1,27 @@
+package com.example.bioms1;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bioms1.databinding.ActivityDemoSessionBinding;
+
+public class demoSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
